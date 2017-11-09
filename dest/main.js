19aa052007d2f6e14903fcd66536eccbe874795c
bohdan_js <bohdan@jaybirdgroup.com>
2017-11-09 21:26:34 +0200

    Change .gitignore and added static compressed files

---
+++ dest/main.js
@@ -0,0 +1,2 @@
+!function(){"use strict";new App({apiUrl:"https://api.flickr.com/services/rest/",secret:"bde8d7724318c876c5f974772dc62e31",rootElement:document.getElementById("app"),imgHeight:366})}();
+//# sourceMappingURL=maps/main.js.map
